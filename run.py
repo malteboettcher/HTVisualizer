743878099e0f315020bc73bd827a00182d4deebb
Malte Böttcher <malte.boettcher@uni-bielefeld.de>
2025-09-08 15:59:46 +0200

    add parameters to application

--- run.py
+++ run.py
@@ -1,9 +1,24 @@
+import argparse
+
 from dash import Dash
 
 from app.layout import create_layout
 
-app = Dash(__name__)
-app.layout = create_layout("data/Thalemine_gene_names.csv", "data/AtRTD3/")
+
+def main(annotation_path, expression_path, host="127.0.0.1", port=8050, debug=True):
+    app = Dash(__name__)
+    app.layout = create_layout(annotation_path, expression_path)
+    app.run(host=host, port=port, debug=debug)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    parser = argparse.ArgumentParser(description="Run Dash gene expression app")
+    parser.add_argument("--annotation", default="data/Thalemine_gene_names.csv",
+                        help="Path to annotation CSV")
+    parser.add_argument("--expression", default="data/AtRTD3/",
+                        help="Path to expression data folder")
+    parser.add_argument("--host", default="127.0.0.1", help="Host to run app on")
+    parser.add_argument("--port", type=int, default=8050, help="Port to run app on")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+
+    args = parser.parse_args()
+    main(args.annotation, args.expression, args.host, args.port, args.debug)
